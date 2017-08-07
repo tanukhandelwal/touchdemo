e981dfda99b5a9005ea406788b620ce3416a75ce
Tanu Khandelwal <tanukhandelwal87@gmail.com>
2017-08-06 18:52:38 -0700

    Add TouchComponent in touchdemo

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
+import HammerComponent from 'react-hammerjs';
 
-import logo from './logo.svg';
 import './App.css';
+
+class CircleComponent extends HammerComponent {
+  render() {
+    console.log('this.props: ', this.props);
+    return (
+      <svg height="100" width="100">
+        <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="1"
+                fill={this.props.color} />
+      </svg>
+    );
+  }
+}
+
+class TouchComponent extends HammerComponent {
+  constructor() {
+    super();
+    this.state = {
+      color: 'red'
+    }
+  }
+  handleTap() {
+    this.setState({
+      color: 'blue'
+    })
+  }
+  render() {
+    return (
+      <div className="TouchCard">
+        <CircleComponent onTap={() => this.handleTap()}
+                         color={this.state.color} />
+      </div>
+    );
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to Gestures</h2>
         </div>
-        <p className="App-intro">
-          To get started on touchdemo, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="App-body">
+          <TouchComponent />
+        </div>
       </div>
     );
   }
